e36e556108bb1f654550dbdc964bebce6ddde6fa
Indra Ardika <indraardika@gmail.com>
2025-07-15 09:43:09 +0800

    update

--- resources/views/administrator/dashboardadmin.blade.php
+++ resources/views/administrator/dashboardadmin.blade.php
@@ -33,12 +33,13 @@
                             <div class="row">
                                 <div class="col-8">
                                     <div class="numbers">
-                                        <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Instansi</p>
+                                        <p class="text-sm mb-0 text-uppercase font-weight-bold">SPJ Masuk</p>
                                         <h5 class="font-weight-bolder">
-                                            1
+                                            10
                                         </h5>
                                         <p class="mb-0">
-                                            <span class="text-success text-sm font-weight-bolder">.</span>
+                                            <span class="text-success text-sm font-weight-bolder">+3</span> since last
+                                            week
 
                                         </p>
                                     </div>
@@ -46,7 +47,7 @@
                                 <div class="col-4 text-end">
                                     <div
                                         class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
-                                        <i class="bi bi-building-fill-add text-lg opacity-10" aria-hidden="true"></i>
+                                        <i class="ni ni-single-copy-04 text-lg opacity-10" aria-hidden="true"></i>
                                     </div>
                                 </div>
                             </div>
@@ -59,12 +60,12 @@
                             <div class="row">
                                 <div class="col-8">
                                     <div class="numbers">
-                                        <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Users</p>
+                                        <p class="text-sm mb-0 text-uppercase font-weight-bold">SPJ LS</p>
                                         <h5 class="font-weight-bolder">
-                                            2,300
+                                            4
                                         </h5>
                                         <p class="mb-0">
-                                            <span class="text-success text-sm font-weight-bolder">+3%</span>
+                                            <span class="text-success text-sm font-weight-bolder">+3</span>
                                             since last week
                                         </p>
                                     </div>
@@ -85,12 +86,12 @@
                             <div class="row">
                                 <div class="col-8">
                                     <div class="numbers">
-                                        <p class="text-sm mb-0 text-uppercase font-weight-bold">New Clients</p>
+                                        <p class="text-sm mb-0 text-uppercase font-weight-bold">SPJ GU</p>
                                         <h5 class="font-weight-bolder">
-                                            +3,462
+                                            5
                                         </h5>
                                         <p class="mb-0">
-                                            <span class="text-danger text-sm font-weight-bolder">-2%</span>
+                                            <span class="text-danger text-sm font-weight-bolder">-2</span>
                                             since last quarter
                                         </p>
                                     </div>
@@ -111,12 +112,12 @@
                             <div class="row">
                                 <div class="col-8">
                                     <div class="numbers">
-                                        <p class="text-sm mb-0 text-uppercase font-weight-bold">Sales</p>
+                                        <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Verif</p>
                                         <h5 class="font-weight-bolder">
-                                            $103,430
+                                            10
                                         </h5>
                                         <p class="mb-0">
-                                            <span class="text-success text-sm font-weight-bolder">+5%</span> than last
+                                            <span class="text-success text-sm font-weight-bolder">+5</span> than last
                                             month
                                         </p>
                                     </div>
@@ -133,7 +134,7 @@
                 </div>
             </div>
             <div class="row mt-4">
-                <div class="col-lg-7 mb-lg-0 mb-4">
+                <div class="col-lg-12 mb-lg-0 mb-4">
                     <div class="card z-index-2 h-100">
                         <div class="card-header pb-0 pt-3 bg-transparent">
                             <h6 class="text-capitalize">Sales overview</h6>
@@ -149,59 +150,11 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-5">
-                    <div class="card card-carousel overflow-hidden h-100 p-0">
-                        <div id="carouselExampleCaptions" class="carousel slide h-100" data-bs-ride="carousel">
-                            <div class="carousel-inner border-radius-lg h-100">
-                                <div class="carousel-item h-100 active" style="background-image: url('../assets/img/carousel-1.jpg');
-      background-size: cover;">
-                                    <div class="carousel-caption d-none d-md-block bottom-0 text-start start-0 ms-5">
-                                        <div class="icon icon-shape icon-sm bg-white text-center border-radius-md mb-3">
-                                            <i class="ni ni-camera-compact text-dark opacity-10"></i>
-                                        </div>
-                                        <h5 class="text-white mb-1">Get started with Argon</h5>
-                                        <p>There’s nothing I really wanted to do in life that I wasn’t able to get good
-                                            at.</p>
-                                    </div>
-                                </div>
-                                <div class="carousel-item h-100" style="background-image: url('../assets/img/carousel-2.jpg');
-      background-size: cover;">
-                                    <div class="carousel-caption d-none d-md-block bottom-0 text-start start-0 ms-5">
-                                        <div class="icon icon-shape icon-sm bg-white text-center border-radius-md mb-3">
-                                            <i class="ni ni-bulb-61 text-dark opacity-10"></i>
-                                        </div>
-                                        <h5 class="text-white mb-1">Faster way to create web pages</h5>
-                                        <p>That’s my skill. I’m not really specifically talented at anything except for
-                                            the ability to learn.</p>
-                                    </div>
-                                </div>
-                                <div class="carousel-item h-100" style="background-image: url('../assets/img/carousel-3.jpg');
-      background-size: cover;">
-                                    <div class="carousel-caption d-none d-md-block bottom-0 text-start start-0 ms-5">
-                                        <div class="icon icon-shape icon-sm bg-white text-center border-radius-md mb-3">
-                                            <i class="ni ni-trophy text-dark opacity-10"></i>
-                                        </div>
-                                        <h5 class="text-white mb-1">Share with us your design tips!</h5>
-                                        <p>Don’t be afraid to be wrong because you can’t learn anything from a
-                                            compliment.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <button class="carousel-control-prev w-5 me-3" type="button"
-                                data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
-                            </button>
-                            <button class="carousel-control-next w-5 me-3" type="button"
-                                data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
-                            </button>
-                        </div>
-                    </div>
-                </div>
             </div>
-            @include('administrator.partials.footer')
+        </div>
+        </div>
+        </div>
+        @include('administrator.partials.footer')
         </div>
     </main>
     <!--   Core JS Files   -->
