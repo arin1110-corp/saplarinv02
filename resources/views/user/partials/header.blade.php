66d385f4bf9a335ef4d9ead09ccd9b379bf3d909
Indra Ardika <indraardika@gmail.com>
2026-06-09 16:02:40 +0800

    UPDATE LOGO

--- resources/views/user/partials/header.blade.php
+++ resources/views/user/partials/header.blade.php
@@ -6,6 +6,7 @@
 <script src="https://cdn.tailwindcss.com"></script>
 
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+<link rel="icon" href="{{ asset('image/pemprov.png') }}" type="image/png">
 
 <style>
     body {
