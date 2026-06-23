469a03bd2e5fb4ebd1cd6c4198593eb1d3bef826
Indra Ardika <indraardika@gmail.com>
2026-06-23 13:55:22 +0800

    ASC

--- resources/views/administrator/spj/index.blade.php
+++ resources/views/administrator/spj/index.blade.php
@@ -895,7 +895,7 @@
             scrollX: true,
             autoWidth: false,
             pageLength: 10,
-            order: [[1, 'desc']],
+            order: [[1, 'asc']],
             language: {
                 search: "Cari:",
                 lengthMenu: "Tampilkan _MENU_ data",
